d489e7f96dc5d9b4e6c6f2f3225fec2d323f5a4b
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2025-03-14 23:12:14 +0600

    integrate postgresSQL

--- task_management/settings.py
+++ task_management/settings.py
@@ -75,12 +75,36 @@ WSGI_APPLICATION = 'task_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+# Database for sqlite
+# ======================
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
+
+# Database for postgresSQL
+# ==========================
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task-management',
+        'USER': 'postgres',
+        'PASSWORD': 'Admin@123',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
+
+
+
 
 
 # Password validation
