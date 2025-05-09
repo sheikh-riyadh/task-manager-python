24e96cd73ef3adb7cf83aa1033d3d22bcc6a159b
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2025-05-09 19:20:18 +0600

    manage environement variable

--- task/models.py
+++ task/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save, m2m_changed
 from django.dispatch import receiver
+from django.core.mail import send_mail
 # Create your models here.
 
 
@@ -79,3 +80,18 @@ def notify_created_task(sender, instance, created, **kwargs):
 @receiver(pre_save, sender=Employee)
 def notify_employee(sender, instance, **kwargs):
     print('Get notified employee')
+
+
+@receiver(m2m_changed, sender=Task.assigned_to.through)
+def send_email_notification_employee(sender, instance, action, **kwargs):
+
+    emails_list = [employee.email for employee in instance.assigned_to.all()]
+
+    if action == 'post_add':
+        send_mail(
+    "New task assigned",
+    f"You have assinged new task : {instance.project.name}",
+    "sheikhriyad350883@gmail.com",
+    emails_list,
+    )
+    
